5288ef831fe0f30951d8b66c119ed25568c5fa7f
Rui Santos <ruicastro_1990@hotmail.com>
2017-11-09 23:39:37 +0000

    Old version

---
+++ Rico/Models/ParameterProperties.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rico.Models
+{
+	class ParameterProperties
+	{
+		public string parameterName { get; set; }
+		public double parameterAverage { get; set; }
+		public int numberOfParametersFound { get; set; }
+		public bool foundParameter { get; set; }
+	}
+}
---
+++ Rico/Models/ParameterValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rico
+{
+	public class ParameterValidation
+	{
+		public string parametersNotFound { get; set; }
+		public string duplicatedParameters { get; set; }
+		public int amountOfParametersNotFound { get; set; }
+		public int amountOfDuplicates { get; set; }
+		//public bool parameterWithNotEnoughChars { get; set; }
+
+		//public bool CheckMinimumAmountOfCharacters(string parameter, int minLength = 2)
+		//{// Return true for enough chars
+		//	var array = parameter.Split(',');
+		//	if (array[0].Length > minLength) return true;
+		//	return false;
+		//}
+	}
+}
